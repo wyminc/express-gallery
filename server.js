3e6dfb81707457c2745a9b1b88faa757dc8a1463
DLeaguer <immostlikely@hotmail.com>
2018-09-27 12:35:29 -1000

    create routes/.hbs files for gallery with knex.raw

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ const bodyParser = require('body-parser');
 
 const PORT = process.env.EXPRESS_CONTAINER_PORT;
 
+const galleryRoute = require('./routes/gallery.js');
+
 app.get("/", (req, res) => {
   res.json("Hi");
 })
 
+app.use('/gallery', galleryRoute)
+
+app.get('*', (req, res) => {
+  res.json('404 error');
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`)
