ace673778bacd5f2af5435214daef0bf0860e15f
DLeaguer <immostlikely@hotmail.com>
2018-09-27 14:59:33 -1000

    update routes/views.hbs files to show gallery in browser

---
+++ routes/gallery.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const Router = express.Router();
+const knex = require('../knex/knex.js');
+
+//RENDER ALL
+Router.get('/', (req, res) => {
+  console.log('Welcome to the Gallery');
+  knex.raw(`SELECT * FROM gallery`)
+    .then( result => {
+      console.log('result.rows[0] =', result.rows[0]);
+      const gallery = result.rows
+      res.render('gallery', { gallery });
+    })
+    .catch( err => {
+      console.log('error', err);
+    });
+});
+
+//RENDER FORM 
+Router.get('/new', (req, res) => {
+    res.render('gallery-form');
+});
+
+Router.get('/:id/edit', (req, res) => {
+  const { id } = req.params;
+  knex.raw(`SELECT * FROM gallery WHERE id = ${id}`)
+    .then( result => {
+      const itemToEdit = result.rows[0]
+      res.render('edit', { itemToEdit });
+    })
+    .catch( err => {
+      console.log('error', err)
+    })
+});
+
+//RENDER DETAIL 
+Router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  knex.raw(`SELECT * FROM gallery WHERE id = ${id}`)
+    .then( result => {
+      const gallery = result.rows[0]
+      res.render('gallery-detail', gallery);
+    })
+    .catch( err => {
+      console.log('error', err);
+    });
+});
+
+//ADD 
+Router.post('/new', (req, res) => {
+  const gallery = req.body;
+  knex.raw(`INSERT INTO gallery (author_id, link, description) VALUES ('${gallery.author_id}', '${gallery.link}', '${gallery.description}')`)
+    .then( results =>  {
+      res.redirect('/');
+    })
+    .catch( err => {
+      console.log('error', err)
+      res.redirect('/');
+    });
+});
+
+//REMOVE  
+Router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  knex.raw(`DELETE FROM gallery WHERE id = ${id}`)
+    .then( result => {
+      res.redirect('/');
+    })
+    .catch( err => {
+      console.log('error', err);
+    });
+});
+
+//EDIT  
+Router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const gallery = req.body;
+  knex.raw(`UPDATE gallery SET author_id = '${gallery.author_id}', link = ${gallery.link}, description = ${gallery.description} WHERE id = ${id}`)
+  .then( result => {
+      res.redirect(`/${id}`);
+    })
+    .catch( err => {
+      console.log('error', err)
+    });
+});
+
+module.exports = Router;
--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
 
 const PORT = process.env.EXPRESS_CONTAINER_PORT;
 
-app.get("/", (req, res) => {
-  res.json("Hi");
-})
+const galleryRoute = require('./routes/gallery.js');
 
+app.engine('.hbs', exphbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+console.log('   connected engine/set');
+
+// app.get("/", (req, res) => {
+//   console.log('start server.js');
+//   res.json("Hi");
+// })
+
+app.use('/', galleryRoute)
+
+app.get('*', (req, res) => {
+  res.json('404 error');
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`)
